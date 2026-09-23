d0ffd9278baf5d680d77245283092e41b839b8dc
Ricardo Maia <ricardomaiadev@gmail.com>
2026-09-23 11:45:10 -0400

    feat: adiciona classe Pessoa

--- src/main/java/br/com/iniflex/Principal.java
+++ src/main/java/br/com/iniflex/Principal.java
@@ -1,8 +1,21 @@
 package br.com.iniflex;
 
+import java.security.Permission;
+import java.time.LocalDate;
+import br.com.iniflex.model.Pessoa;
+
 public class Principal {
     
     public static void main(String[] args) {
-        System.out.println("Teste Pratico Iniflex");
+
+        // System.out.println("Teste Pratico Iniflex");
+
+        Pessoa pessoa = new Pessoa(
+            "Maria",
+            LocalDate.of(2000, 10, 18)
+        );
+
+        System.out.println(pessoa.getNome());
+        System.out.println(pessoa.getDataNascimento());
     }
 }
