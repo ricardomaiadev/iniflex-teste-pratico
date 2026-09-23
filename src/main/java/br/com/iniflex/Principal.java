e8af3b4d72e594149a847e495dff7dac59e77297
Ricardo Maia <ricardomaiadev@gmail.com>
2026-09-23 12:30:39 -0400

    feat: adiciona classe Funcionario

--- src/main/java/br/com/iniflex/Principal.java
+++ src/main/java/br/com/iniflex/Principal.java
@@ -1,8 +1,8 @@
 package br.com.iniflex;
 
-import java.security.Permission;
+import java.math.BigDecimal;
 import java.time.LocalDate;
-import br.com.iniflex.model.Pessoa;
+import br.com.iniflex.model.Funcionario;
 
 public class Principal {
     
@@ -10,12 +10,16 @@ public class Principal {
 
         // System.out.println("Teste Pratico Iniflex");
 
-        Pessoa pessoa = new Pessoa(
+        Funcionario maria = new Funcionario(
             "Maria",
-            LocalDate.of(2000, 10, 18)
+            LocalDate.of(2000, 10, 18),
+            new BigDecimal("2009.44"),
+            "Operador"
         );
-
-        System.out.println(pessoa.getNome());
-        System.out.println(pessoa.getDataNascimento());
+        
+        System.out.println(maria.getNome());
+        System.out.println(maria.getDataNascimento());
+        System.out.println(maria.getSalario());
+        System.out.println(maria.getFuncao());
     }
 }
