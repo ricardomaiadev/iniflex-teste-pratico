693c1807bb7954fa8a657b610f911918575501b3
Ricardo Maia <ricardomaiadev@gmail.com>
2026-09-23 12:58:02 -0400

    feat: cadastra funcionarios

--- src/main/java/br/com/iniflex/Principal.java
+++ src/main/java/br/com/iniflex/Principal.java
@@ -2,24 +2,83 @@ package br.com.iniflex;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import br.com.iniflex.model.Funcionario;
 
 public class Principal {
     
     public static void main(String[] args) {
 
-        // System.out.println("Teste Pratico Iniflex");
+        List<Funcionario> funcionarios = new ArrayList<>();
 
-        Funcionario maria = new Funcionario(
+        funcionarios.add(new Funcionario(
             "Maria",
             LocalDate.of(2000, 10, 18),
             new BigDecimal("2009.44"),
             "Operador"
-        );
-        
-        System.out.println(maria.getNome());
-        System.out.println(maria.getDataNascimento());
-        System.out.println(maria.getSalario());
-        System.out.println(maria.getFuncao());
+        ));
+
+        funcionarios.add(new Funcionario(
+            "João",
+            LocalDate.of(1990, 5, 12),
+            new BigDecimal("2284.38"),
+            "Operador"
+        ));
+        funcionarios.add(new Funcionario(
+            "Caio",
+            LocalDate.of(1961, 5, 2),
+            new BigDecimal("9836.14"),
+            "Coordernador"
+        ));
+        funcionarios.add(new Funcionario(
+            "Miguel",
+            LocalDate.of(1988, 10, 14),
+            new BigDecimal("19119.88"),
+            "Diretor"
+        ));
+        funcionarios.add(new Funcionario(
+            "Alice",
+            LocalDate.of(1995, 1, 5),
+            new BigDecimal("2234.68"),
+            "Recepcionista"
+        ));
+        funcionarios.add(new Funcionario(
+            "Heitor",
+            LocalDate.of(1999, 11, 19),
+            new BigDecimal("1582.72"),
+            "Operador"
+        ));
+        funcionarios.add(new Funcionario(
+            "Arthur",
+            LocalDate.of(1993, 3, 31),
+            new BigDecimal("4071.84"),
+            "Contador"
+        ));
+        funcionarios.add(new Funcionario(
+            "Laura",
+            LocalDate.of(1994, 7, 8),
+            new BigDecimal("3017.45"),
+            "Gerente"
+        ));
+        funcionarios.add(new Funcionario(
+            "Heloísa",
+            LocalDate.of(2003, 5, 24),
+            new BigDecimal("1606.85"),
+            "Eletricista"
+        ));
+        funcionarios.add(new Funcionario(
+            "Helena",
+            LocalDate.of(1996, 9, 2),
+            new BigDecimal("2799.93"),
+            "Gerente"
+        ));
+
+        System.out.println("Quantidade de funcionários: " + funcionarios.size());
+
+        for(Funcionario funcionario : funcionarios){
+            System.out.println(funcionario.getNome());
+        }
     }
 }
